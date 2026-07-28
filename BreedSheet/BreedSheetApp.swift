efd274b14922ef89b3b3cf4ec85bd2d762d061cf
Pim <pim@pixelrock.nl>
2026-07-28 11:14:45 +0200

    Initial Commit

---
+++ BreedSheet/BreedSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedSheetApp.swift
+//  BreedSheet
+//
+//  Created by Pim on 28/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BreedSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
